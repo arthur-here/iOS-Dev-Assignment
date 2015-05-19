14706127f37e3f919d4e59a967141f2bceaf2aa1
burntheroad <gibslp69@gmail.com>
2015-05-19 22:12:40 +0300

    Initial Commit

---
+++ iOS Dev AssignmentTests/iOS_Dev_AssignmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_Dev_AssignmentTests.swift
+//  iOS Dev AssignmentTests
+//
+//  Created by Arthur Myronenko on 19.05.15.
+//  Copyright (c) 2015 Arthur Myronenko. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iOS_Dev_AssignmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
